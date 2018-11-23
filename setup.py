05a6f613c5b6970e4eb92bdace781cbad64f69e8
Libor Wagner <libor.wagner@cvut.cz>
2018-11-23 12:51:16 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='pyreport',
+      version='0.1',
+      description='Simple reports from python',
+      author='Libor Wagner',
+      author_email='libor.wagner@cvut.cz',
+      url='https://gitlab.ciirc.cvut.cz/b635-incubator/pyreport',
+      packages=['pyreport'],
+      package_dir={'pyreport': 'src/'},
+      install_requires=[
+            'jinja2'
+          ]
+      )
